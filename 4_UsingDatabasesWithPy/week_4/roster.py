5a9233731636500b141c05de5cd680ae7a4f8430
citizen333 <andrey.kalinin.mail@gmail.com>
2018-01-10 06:39:04 +0300

    Add courses 4 and 5

--- 4_UsingDatabasesWithPy/week_4/roster.py
+++ 4_UsingDatabasesWithPy/week_4/roster.py
@@ -1,18 +1,15 @@
 import json
 import sqlite3
 
-conn = sqlite3.connect('rosterdb_w3_a1.sqlite')
+conn = sqlite3.connect('rosterdb.sqlite')
 cur = conn.cursor()
 
-# Drop tables if they exist
+# Do some setup
 cur.executescript('''
 DROP TABLE IF EXISTS User;
 DROP TABLE IF EXISTS Member;
-DROP TABLE IF EXISTS Course
-''')
+DROP TABLE IF EXISTS Course;
 
-# Create tables
-cur.executescript('''
 CREATE TABLE User (
     id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
     name   TEXT UNIQUE
@@ -33,7 +30,7 @@ CREATE TABLE Member (
 
 fname = input('Enter file name: ')
 if len(fname) < 1:
-    fname = 'roster_data.json'
+    fname = 'roster_data_sample.json'
 
 # [
 #   [ "Charley", "si110", 1 ],
@@ -46,9 +43,8 @@ for entry in json_data:
 
     name = entry[0];
     title = entry[1];
-    role = entry[2];
 
-    print((name, title, role))
+    print((name, title))
 
     cur.execute('''INSERT OR IGNORE INTO User (name)
         VALUES ( ? )''', ( name, ) )
@@ -61,17 +57,7 @@ for entry in json_data:
     course_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR REPLACE INTO Member
-        (user_id, course_id, role) VALUES ( ?, ?, ? )''',
-        ( user_id, course_id, role ) )
+        (user_id, course_id) VALUES ( ?, ? )''',
+        ( user_id, course_id ) )
 
-conn.commit()
-
-sqlstr = '''
-SELECT hex(User.name || Course.title || Member.role ) AS X FROM 
-    User JOIN Member JOIN Course 
-    ON User.id = Member.user_id AND Member.course_id = Course.id
-    ORDER BY X
-'''
-
-for row in cur.executescript(sqlstr):
-    print(row[0])
+    conn.commit()
